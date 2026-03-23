3f7e8b8f421741af6487660e1acfa1891fa96664
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-03-23 06:24:14 +0100

    Update player.py

--- app/players/player.py
+++ app/players/player.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
 
 class Player(ABC):
-    def __init__(self, nickname):
+    def __init__(self, nickname: str):
         self.nickname = nickname
 
 
     @abstractmethod
-    def get_rating(self):
+    def get_rating(self) -> int:
         pass
     @abstractmethod
 
-    def player_info(self):
+    def player_info(self) -> str:
         pass
