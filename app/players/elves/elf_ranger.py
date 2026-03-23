46d1b688ef90351f4c5389210cfd8374ed9b99c3
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-03-23 06:23:12 +0100

    Update elf_ranger.py

--- app/players/elves/elf_ranger.py
+++ app/players/elves/elf_ranger.py
@@ -10,4 +10,4 @@ class ElfRanger(Elf):
         return 3 * self._bow_level
 
     def player_info(self):
-        return f"Elf Ranger {self.nickname}"
+        return f"Elf ranger {self.nickname}. {self.nickname} has bow of the {self._bow_level} level"
