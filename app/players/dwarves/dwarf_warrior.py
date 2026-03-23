24034b991d1b7fff5b8f27722ee782f5a3ed2313
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-03-23 06:32:53 +0100

    Update dwarf_warrior.py

--- app/players/dwarves/dwarf_warrior.py
+++ app/players/dwarves/dwarf_warrior.py
@@ -5,8 +5,8 @@ class DwarfWarrior(Dwarf):
         super().__init__(nickname, favourite_dish)
         self._hummer_level = hummer_level
 
-    def player_info(self):
-        return f"Dwarf warrior {self.nickname}. {self.nickname} has a hummer of the {self.hummer_level} level"
+    def player_info(self) -> str:
+        return f"Dwarf warrior {self.nickname}. {self.nickname} has a hummer of the {self._hummer_level} level"
 
-    def get_rating(self):
+    def get_rating(self) -> int:
         return self._hummer_level + 4
