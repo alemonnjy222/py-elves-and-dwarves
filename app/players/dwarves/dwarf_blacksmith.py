2ac99b577adfc94994b0b8b8c2a1b3f2cc5dfee1
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-03-23 06:20:44 +0100

    Update dwarf_blacksmith.py

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -6,6 +6,6 @@ class DwarfBlacksmith(Dwarf):
         self._skill_level = skill_level
 
     def player_info(self):
-        print(f"Dwarf blacksmith {self.nickname} with skill of the {self.skill_level} level")
+        print(f"Dwarf blacksmith {self.nickname} with skill of the {self._skill_level} level")
     def get_rating(self):
         return self._skill_level
